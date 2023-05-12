fd2b38742b0f7d98ede8100bc93f322257361c96
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-05-12 16:39:59 +0530

    changes

--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -1,11 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
-const { createBill,
-    updateBill,
-    markBillAsReceived,getBillById,
-    addPayment, getAllBills,
-    editBill } = require('../controllers/billController')
+const { createBill,updateBill,markBillAsReceived,getBillById,addPayment,searchBill, getAllBills,getTotalExpenseAmount,editBill } = require('../controllers/billController')
 
 const { protect } = require('../middleware/authMiddleware')
 
@@ -13,6 +9,8 @@ router.post('/',protect, createBill);
 router.put('/:billId',protect, updateBill);
 router.put('/:billId/received',protect, markBillAsReceived);
 router.put('/:billId/payment',protect, addPayment);
+router.get('/total', protect, getTotalExpenseAmount);
+router.get('/search', protect, searchBill);
 router.get('/:id', protect, getBillById);
 router.get('/', protect, getAllBills);
 router.put('/edit/:billId', protect, editBill);
